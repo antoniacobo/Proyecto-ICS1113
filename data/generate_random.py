7d57bc61d2072cb955c4659e13421f78947a0145
Ariel Martínez <amartinez5@uc.cl>
2019-04-28 15:48:02 -0400

    update data

--- data/generate_random.py
+++ data/generate_random.py
@@ -1,13 +1,13 @@
 from sys import argv
 from random import randint, choice
-from json import load
+from json import load as json_load
 
 
 def generate_products(products_n: int, season_duration: int):
     with open(PATHS["PRODUCTS"], 'w') as file:
         file.write('id,volume(cm^2),season_duration\n')
         for i in range(products_n):
-            file.write(f'{i},'
+            file.write(f'p{i},'
                        f'{randint(10, 1000)},'
                        f'{season_duration}\n')
 
@@ -16,7 +16,7 @@ def generate_stores(stores_n: int):
     with open(PATHS["STORES"], 'w') as file:
         file.write('id,capacity(m^2)\n')
         for i in range(stores_n):
-            file.write(f'{i},'
+            file.write(f's{i},'
                        f'{randint(10, 1000)}\n')
 
 
@@ -24,7 +24,7 @@ def generate_transportation(transportation_n: int):
     with open(PATHS["TRANSPORTATION"], 'w') as file:
         file.write('id,capacity(m^2),fix_price,var_price,from,to\n')
         for i in range(transportation_n):
-            file.write(f'{i},'
+            file.write(f't{i},'
                        f'{randint(1, 15)}'
                        f'{randint(5000, 10000)},'
                        f'{randint(1000, 20000)},'
@@ -39,8 +39,8 @@ def generate_demand(periods_n: int, stores_n: int, products_n: int):
             for j in range(stores_n):
                 for k in range(products_n):
                     file.write(f'{i},'
-                               f'{j},'
-                               f'{k},'
+                               f's{j},'
+                               f'p{k},'
                                f'{randint(0, 500)}\n')
 
 
@@ -51,7 +51,7 @@ def generate_arrivals(periods_n: int, products_n: int, season_duration: int):
             for j in range(products_n):
                 amount = 0 if i % season_duration else randint(0, 5000)
                 file.write(f'{i},'
-                           f'{j},'
+                           f'p{j},'
                            f'{amount}\n')
 
 
@@ -66,5 +66,5 @@ def generate_data(periods_n: int, products_n: int, stores_n: int,
 
 if __name__ == "__main__":
     with open(argv[1], 'r') as file:
-        PATHS = load(file)
+        PATHS = json_load(file)
     generate_data(*map(int, argv[2:]))
