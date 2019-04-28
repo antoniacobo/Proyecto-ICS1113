b203b705ddb9bf5ce6d441556c946281485a783c
Ariel Martínez <amartinez5@uc.cl>
2019-04-28 17:39:41 -0400

    simple model

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 from gurobipy import Model, GRB
+from json import load as json_load
+from parameters import ParametersContainer
+from variables import VariablesContainer
 
 
 if __name__ == "__main__":
+    with open('data/PATHS.json') as file:
+        PATHS = json_load(file)
+
+    parameters = ParametersContainer(PATHS)
+    variables = VariablesContainer(parameters)
+
     model = Model('Guess')
+    variables.set_all(model)
+
+    model.update()
+    model.optimize()
+
